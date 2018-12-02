69da534cb856c3b74f99ac1b86884b648a7c28fd
miraoto <miraoto@gmail.com>
2018-12-02 23:01:10 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const request = require('request');
+const syncRequest = require('sync-request');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+// for debug
+app.get('/', (req, res) => {
+  return '今日は昨日より寒いです';
+});
+
+app.post('/', (req, res) => {
+  return '今日は昨日より寒いです';
+});
